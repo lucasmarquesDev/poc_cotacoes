937a0a17c27d8db663e50a7c3f029ca61fba820c
lucasmarquesDev <lunatec09@gmail.com>
2025-02-10 09:20:27 -0300

    initial commit

---
+++ POC_COTACOES.API/Controllers/CotacaoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace POC_COTACOES.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CotacaoController : ControllerBase
+    {
+        [HttpGet("buscar-cotacao")]
+        public async Task<IActionResult> GetCotacao([FromQuery] string moeda)
+        {
+            return Ok();
+        }
+    }
+}
